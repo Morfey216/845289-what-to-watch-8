8e91c3b32f0316869bb496c54720b621e605c034
Morfey216 <Morfey4web@gmail.com>
2021-10-24 01:03:30 +0300

    adds a Not Found page

---
+++ project/src/components/pages/not-found/not-found.tsx
@@ -0,0 +1,11 @@
+function NotFound(): JSX.Element {
+  return (
+    <div>
+      <h2>Error<span>404</span></h2>
+      <p>Sorry, the page you requested was not found.</p>
+      <a href="/">Go to Main page</a>
+    </div>
+  );
+}
+
+export default NotFound;
