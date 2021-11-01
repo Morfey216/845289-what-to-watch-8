c0df0218376e7e0551a3b9b8b13307401ae771ad
Morfey216 <Morfey4web@gmail.com>
2021-11-01 08:01:38 +0300

    adds routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import AddReview from '../pages/add-review/add-review';
+import Film from '../pages/film/film';
 import Main from '../pages/main/main';
+import MyList from '../pages/my-list/my-list';
+import NotFound from '../pages/not-found/not-found';
+import Player from '../pages/player/player';
+import SignIn from '../pages/sign-in/sign-in';
 
 type AppProps = {
   title: string,
@@ -8,11 +16,35 @@ type AppProps = {
 
 function App({title, genre, release}: AppProps): JSX.Element {
   return (
-    <Main
-      title = {title}
-      genre = {genre}
-      release = {release}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route path = {AppRoute.Main} exact>
+          <Main
+            title = {title}
+            genre = {genre}
+            release = {release}
+          />
+        </Route>
+        <Route path={AppRoute.SignIn} exact>
+          <SignIn />
+        </Route>
+        <Route path={AppRoute.MyList} exact>
+          <MyList/>
+        </Route>
+        <Route path={AppRoute.Film} exact>
+          <Film/>
+        </Route>
+        <Route path={AppRoute.AddReview} exact>
+          <AddReview/>
+        </Route>
+        <Route path={AppRoute.Player} exact>
+          <Player/>
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
