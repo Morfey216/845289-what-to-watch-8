3faa75383077a0113b118217a5a379fba8a635c1
Morfey216 <Morfey4web@gmail.com>
2021-11-08 03:26:24 +0300

    adds state for the active movie card

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -2,16 +2,22 @@ import { Film } from '../../types/films';
 
 type FilmCardProps = {
   film: Film,
+  setActiveFilm(id: number): void,
 }
 
-function FilmCard({film}: FilmCardProps): JSX.Element {
+function FilmCard({film, setActiveFilm}: FilmCardProps): JSX.Element {
+  const {id, name, previewImage} = film;
+
   return (
-    <article className="small-film-card catalog__films-card">
+    <article className="small-film-card catalog__films-card"
+      onMouseOver={() => setActiveFilm(id)}
+      onMouseOut={() => setActiveFilm(0)}
+    >
       <div className="small-film-card__image">
-        <img src={film.previewImage} alt={film.name} width="280" height="175" />
+        <img src={previewImage} alt={film.name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">{film.name}</a>
+        <a className="small-film-card__link" href="film-page.html">{name}</a>
       </h3>
     </article>
   );
--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Films } from '../../types/films';
 import FilmCard from '../film-card/film-card';
 
@@ -6,12 +7,15 @@ type FilmsProps = {
 }
 
 function FilmsList({films}: FilmsProps): JSX.Element {
+  const [, setActiveFilm] = useState(0);
+
   return (
     <div className="catalog__films-list">
       {films.map((film) => (
         <FilmCard
           key = {film.id}
           film = {film}
+          setActiveFilm = {setActiveFilm}
         />
       ))}
     </div>
