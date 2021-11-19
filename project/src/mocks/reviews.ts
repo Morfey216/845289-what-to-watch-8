e112d039a2bf09cd4e712fff0abf9c9839506f9e
Morfey216 <Morfey4web@gmail.com>
2021-11-19 10:16:12 +0300

    adds reviews mocks

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,34 @@
+const reviews = [
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'Kate Muir',
+    },
+    rating: 8.9,
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director.',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+  {
+    id: 2,
+    user: {
+      id: 5,
+      name: 'Matthew Lickona',
+    },
+    rating: 7.2,
+    comment: 'The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.',
+    date: '2020-06-08T14:13:56.569Z',
+  },
+  {
+    id: 3,
+    user: {
+      id: 3,
+      name: 'Paula Fleri-Soler',
+    },
+    rating: 7.9,
+    comment: 'It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.',
+    date: '2021-09-06T14:13:56.569Z',
+  },
+];
+
+export {reviews};
