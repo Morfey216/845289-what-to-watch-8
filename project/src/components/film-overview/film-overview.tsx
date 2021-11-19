a42919ff213d5e29a91717bde16de79247ae5e7e
Morfey216 <Morfey4web@gmail.com>
2021-11-19 10:01:37 +0300

    adds FilmOverview component

---
+++ project/src/components/film-overview/film-overview.tsx
@@ -0,0 +1,36 @@
+import { MAX_NUMBER_OF_ACTORS } from '../../const';
+import { Film } from '../../types/films';
+
+type FilmOverviewProps = {
+  film: Film;
+}
+
+function FilmOverview({film}: FilmOverviewProps):JSX.Element {
+  const {rating, scoresCount, director, starring} = film;
+
+  return (
+    <>
+      <div className="film-rating">
+        <div className="film-rating__score">{rating}</div>
+        <p className="film-rating__meta">
+          <span className="film-rating__level">Very good</span>
+          <span className="film-rating__count">{scoresCount} ratings</span>
+        </p>
+      </div>
+
+      <div className="film-card__text">
+        <p>{film.description}</p>
+        <p className="film-card__director"><strong>Director: {director}</strong></p>
+        <p className="film-card__starring">
+          <strong>
+            Starring: {film.starring.length > MAX_NUMBER_OF_ACTORS
+              ? `${starring.slice(0, MAX_NUMBER_OF_ACTORS).map((it) => it).join(', ')} and other`
+              : `${starring.map((it) => it).join(', ')}`}
+          </strong>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default FilmOverview;
